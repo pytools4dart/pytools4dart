5bfbe5cc8d324f1b18d2e98d14848978a9dbcfb6
eric.chraibi <eric.chraibi@irstea.fr>
2018-06-28 15:43:22 +0200

    Fixes init!

--- pytools4dart/xmlwriters/__init__.py
+++ pytools4dart/xmlwriters/__init__.py
@@ -9,7 +9,20 @@ to be called this way :
     from xmlwriters import *
 
 """
+from atmosphere import write_atmosphere
+from coeff_diff import write_coeff_diff
+from directions import write_directions
+from inversion import write_inversion
+from maket import write_maket
+from object_3d import write_object_3d
+from phase import write_phase
+from plots import write_plots
+from sequence import write_sequence
+from trees import write_trees
+from urban import write_urban
+from water import write_water
+
+
 __all__ = ['atmosphere', 'coeff_diff', 'directions', 'inversion',
            'maket', 'object_3d', 'phase', 'plots', 'sequence', 'trees',
            'urban', 'water']
-#from atmosphere import write_atmosphere
