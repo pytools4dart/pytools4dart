05e3473316b6fbfcfb0619938bd95bdc7ff82c88
Eric Chraibi <eric.chraibi@irstea.fr>
2018-07-06 14:48:24 +0200

    Add license everywhere

--- pytools4dart/xmlwriters/__init__.py
+++ pytools4dart/xmlwriters/__init__.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  28 14:55:58 2018
-
-@author: mtd
-
+===============================================================================
+# PROGRAMMERS:
+#
+# Eric Chraibi <eric.chraibi@irstea.fr>  -
+https://gitlab.irstea.fr/florian.deboissieu/pytools4dart
+#
+# Copyright 2018 Eric Chraibi
+#
+# This file is part of the pytools4dart package.
+#
+# pytools4dart is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+#
+===============================================================================
 to be called this way :
     from xmlwriters import *
 
--- pytools4dart/xmlwriters/water.py
+++ pytools4dart/xmlwriters/water.py
@@ -1,9 +1,31 @@
 # !/usr/bin/env python2
 #  -*- coding: utf-8 -*-
 """
-Created on Fri Jun  1 14:15:03 2018
-
-@author: mtd
+===============================================================================
+# PROGRAMMERS:
+#
+# Eric Chraibi <eric.chraibi@irstea.fr>  -
+https://gitlab.irstea.fr/florian.deboissieu/pytools4dart
+#
+# Copyright 2018 Eric Chraibi
+#
+# This file is part of the pytools4dart package.
+#
+# pytools4dart is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+#
+===============================================================================
 
 Objects and functions necessary to write the water xml file.
 It is important to note the resulting xml file is written over a single line.
