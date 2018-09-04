e17194f5f707a757c05bc9310ec63d4fe04de006
Eric Chraibi <eric.chraibi@irstea.fr>
2018-09-04 16:23:27 +0200

    Change install requires. Test install works.

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['setuptools','geopandas','lxml'],
 
     # List additional groups of dependencies here (e.g. dev dependencies).
     # You can install these using the following syntax, for example:
