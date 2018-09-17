778f092f2ff84512ab5085d36929e161fd40d7f6
Florian de Boissieu <fdeboiss@gmail.com>
2018-09-17 21:05:48 +0200

    update use_case_2

--- pytools4dart/tests/use_case_2.py
+++ pytools4dart/tests/use_case_2.py
@@ -6,24 +6,41 @@ inventory_file = '/home/boissieu/Florian/IRSTEA/Software/DART_5-7-1_v1061/databa
 inventory = pd.read_csv(inventory_file, comment='*', sep='\t')
 
 simu = pt4d.simulation('use_case_2')
+simu.set_scene_size([40, 40])
 
 print(inventory)
+
+for wvl in np.linspace(0.4, 1.4, 11):
+    simu.addband([wvl, 0.01])
+
 simu.addtrees(inventory)
-simu.addtreespecie(idspecie = 0, lai=-0.25, holes=None,
+simu.addtreespecies(species_id = 0, lai=-0.25, holes=0,
                    trunkopt='trunk',
                    trunktherm='ThermalFunction290_310',
                    vegopt='leafs',
                    vegtherm='ThermalFunction290_310')
+simu.addtreespecies(species_id = 1, lai=-0.1, holes=0,
+                   trunkopt='trunk',
+                   trunktherm='ThermalFunction290_310',
+                   vegopt='leafs',
+                   vegtherm='ThermalFunction290_310')
+
+# simu.addopt(['lambertian', 'ground', 'Lambertian_mineral.db',
+#              'clay_brown', 0])
+
 simu.addopt(['lambertian', 'trunk', 'Lambertian_vegetation.db',
              'bark_deciduous', 0])
 
+# simu.addopt(['vegetation', 'leafs', 'Vegetation.db',
+#              'leaf_deciduous', 1])
+
 dic = {'CBrown': 0.0, 'Cab': range(0,110,10), 'Car': 10,
-           'Cm': 0.01, 'Cw': 0.01, 'N': 2, 'anthocyanin': 1}
+           'Cm': 0.01, 'Cw': 0.012, 'N': 1.8, 'anthocyanin': 0}
 
 simu.addprospectsequence(dic, 'leafs', name='prospect_sequence')
 
-simu.addsequence({'wvl':np.linspace(.4,1.4,11)})
 simu.write_xmls()
+simu.run.sequence('prospect_sequence')
 
 
 
