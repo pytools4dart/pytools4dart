4f87e4df5f714bc75ddfbb9f278ea650950f2bb4
Claudia Lavalley <claudia.lavalley@cirad.fr>
2018-09-20 13:38:46 +0200

    complete use_case_0

--- pytools4dart/examples/use_case_0.py
+++ pytools4dart/examples/use_case_0.py
@@ -4,14 +4,16 @@ import pytools4dart as ptd
 
 # create an empty simulation
 simu = ptd.simulation(name = 'use_case_0')
+
 # define scene size
 scene_dims = [40,40]
 simu.set_scene_size(scene_dims)
+
 # add spectral bands, e.g. 0.485, 0.555, 0.655 nm
 # with 0.07 full width at half maximum
 simu.add_bands({'wvl':[0.485, 0.555, 0.655], 'fwhm':0.07})
 
-# define vegetation optical properties (VOP)
+# define vegetation optical properties (VOP): here using the default veg opt property suggested by DART interface
 opt_prop_ident = 'Turbid_Leaf_Deciduous_Phase_Function'
 veg_opt_prop = ['vegetation', opt_prop_ident, 'Vegetation.db',
             'leaf_deciduous', 1]
@@ -28,4 +30,5 @@ simu.run.full()
 #
 # databases with SQLite database browser
 # generate RGB image (ENVI or PNG formats)
+simu.run.colorCompositeBands(red=2, green=1, blue=0, iteration='X', outdir='rgb')
 # open simulation in DART
