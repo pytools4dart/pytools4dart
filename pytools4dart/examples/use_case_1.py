7e2a873e25172bacc040efa53094bafc381cc141
Florian de Boissieu <fdeboiss@gmail.com>
2018-09-21 14:52:46 +0200

    update use_case_1 with plot

--- pytools4dart/examples/use_case_1.py
+++ pytools4dart/examples/use_case_1.py
@@ -7,7 +7,7 @@ simu = ptd.simulation(name='use_case_1')
 
 simu.add_bands({'wvl':[0.485, 0.555, 0.655], 'fwhm':0.07})
 
-dic = {'CBrown': 0.0, 'Cab': range(0,30,10), 'Car': 10,
+dic = {'CBrown': 0.0, 'Cab': range(0,30,10), 'Car': 50,
            'Cm': 0.01, 'Cw': 0.01, 'N': 2, 'anthocyanin': 1}
 
 simu.add_prospect_sequence(dic, 'proprieteoptpros', name='prospect_sequence')
@@ -15,7 +15,40 @@ simu.add_single_plot(opt_name='proprieteoptpros')
 
 simu.add_sequence({'wvl':np.linspace(.4,.8,5)})
 
+print(simu)
+
 simu.write_xmls()
+simu.run.full()
 simu.run.sequence('prospect_sequence')
+
+
+
+
+import numpy as np
+import pandas as pd
+import sqlite3
+import matplotlib
+simu.get_sequence_db_path("prospect_sequence")
+
+conn = sqlite3.connect(simu.get_sequence_db_path("prospect_sequence"))
+
+c=conn.cursor()
+
+# Create table
+result=[]
+for row in c.execute('''select  idCombination,  valueCentralWavelength, valueResult
+from ScalarResult, DirectionalResult, Reflectance, SpectralBand
+where directionalResult.IdViewAngle = 1
+and scalarResult.idSpectralBand = SpectralBand.idSpectralBand
+and scalarResult.IdScalarResult = DirectionalResult.IdScalarResult
+and DirectionalResult.idDirectionalResult = reflectance.idDirectionalResult
+group by idCombination, valueCentralWavelength
+'''):
+    result.append(row)
+
+arr = np.array(result)
+df = pd.DataFrame(result, columns=['id', 'wavelength', 'reflectance'])
+df.set_index('wavelength', inplace=True)
+df.groupby('id')['reflectance'].plot(legend=True)
 
 
