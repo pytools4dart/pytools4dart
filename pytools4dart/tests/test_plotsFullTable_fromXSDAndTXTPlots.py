b277c6e0b4abd048bdfc649c60d7f0d0bdd39747
Florian de Boissieu <fdeboiss@gmail.com>
2019-02-04 06:49:38 +0100

    for the record

--- pytools4dart/tests/test_plotsFullTable_fromXSDAndTXTPlots.py
+++ pytools4dart/tests/test_plotsFullTable_fromXSDAndTXTPlots.py
@@ -4,9 +4,8 @@ from pytools4dart.simulation import simulation
 
 simu = simulation("test2plotsSimu")
 
-simu.add.opt_property(opt_prop_type="vegetation",opt_prop_name="veg1")
-src_file_path = "plots.txt" #in this file 2 species are requested
-simu.add.plotstxtfile_reference(src_file_path=src_file_path)
+simu.add.optical_property(type="vegetation",ident="veg1")
+simu.add.plots('plots.txt')
 
 simu.core.update()
 
