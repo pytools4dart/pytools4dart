bb568751e639dec81102c0a3c2e3e8030e90a29a
Florian de Boissieu <fdeboiss@gmail.com>
2019-02-22 16:00:28 +0100

    update test_simuWithObj

--- pytools4dart/tests/test_simuWithObj.py
+++ pytools4dart/tests/test_simuWithObj.py
@@ -4,14 +4,22 @@ import pytools4dart as ptd
 # import lxml.etree as etree
 import pandas as pd
 
-simu = ptd.simulation()
-simu.core.maket.Maket.Scene.SceneDimensions.x=10
-simu.core.maket.Maket.Scene.SceneDimensions.y=10
-simu.core.maket.Maket.Scene.CellDimensions.x=0.1
-simu.core.maket.Maket.Scene.CellDimensions.y=0.1
-simu.core.maket.Maket.Soil.OpticalPropertyLink.ident="loam_gravelly_brown_dark_clean_smooth"
-simu.core.maket.Maket.Soil.OpticalPropertyLink.indexFctPhase=2
-merisier = simu.add.object_3d('/home/boissieu/DART_5-7-4_v1083/database/3D_Objects/Tree/Accurate_Trees/Cherry_tree/Merisier_Adulte.obj',
+simu = ptd.simulation('test_simuWithObj', empty=True)
+
+# simu.core.maket.Maket.Scene.SceneDimensions.x=10
+# simu.core.maket.Maket.Scene.SceneDimensions.y=10
+# simu.core.maket.Maket.Scene.CellDimensions.x=0.1
+# simu.core.maket.Maket.Scene.CellDimensions.y=0.1
+
+simu.scene.size = [10, 10]
+simu.scene.cell = [.1, .1]
+
+simu.scene.ground.OpticalPropertyLink.ident="loam_gravelly_brown_dark_clean_smooth"
+# simu.core.maket.Maket.Soil.OpticalPropertyLink.indexFctPhase=2
+obj = '/home/boissieu/DART_5-7-4_v1083/database/3D_Objects/Tree/Accurate_Trees/Cherry_tree/Merisier_Adulte.obj'
+# obj = '/home/boissieu/Scripts/LEAF-EXPEVAL/data/cibles/obj/Cible_01.obj'
+
+merisier = simu.add.object_3d(obj,
                               xpos=5, ypos=5, zpos=0,
                               xscale=3, yscale=3, zscale=3)
 
@@ -37,9 +45,10 @@ simu.add.optical_property(type='lambertian',
                           ident = 'loam_gravelly_brown_dark_clean_smooth')
 
 
-simu.add.sp_bands_uf([[0.485, 0.07],
-                      [0.555, 0.07],
-                      [0.655, 0.07]])
+# Define bands
+for wvl in [0.485, 0.555, 0.655]:
+    simu.add.band(wvl=wvl, bw=0.07)
+
 merisier_op = pd.DataFrame(dict(group = ['TrunkAndBranches', 'Leaves'], ident=['bark_spruce', 'leaf_deciduous'], indexFctPhase = [1, 2]))
 merisier_op.set_index('group', inplace=True)
 for i, g in enumerate(merisier.Groups.Group):
@@ -52,11 +61,9 @@ for i, g in enumerate(merisier.Groups.Group):
     bop.ident = merisier_op.loc[g.name].ident
     bop.indexFctPhase = merisier_op.loc[g.name].indexFctPhase
 
-simu.name = 'test_simuWithObj'
-
 simu.write(overwrite=True)
 
 simu.run.full()
-simu.run.colorCompositeBands(red=3, green=2, blue=1, iteration='X', outdir='rgb')
+simu.run.colorCompositeBands(red=2, green=1, blue=0, iteration='X', outdir='rgb')
 # see rgb images in test_simuWithObj/output/rgb
 
