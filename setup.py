50eec80a745f0cba87ba25c49be26c68525ea183
Florian de Boissieu <fdeboiss@gmail.com>
2019-11-12 12:25:42 +0100

    update setup with python 3

--- setup.py
+++ setup.py
@@ -86,8 +86,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
@@ -96,7 +96,7 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # find_packages(exclude=['doc', 'tests', 'scripts', 'examples']),
-    packages=find_packages(exclude=['doc', 'tests', 'scripts', 'examples']),
+    packages=find_packages(exclude=['docs', 'tests', 'scripts', 'examples']),
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
