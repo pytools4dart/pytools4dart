24f79bc5b3aeb110fb5283d32d46a8b8035888ca
floriandeboissieu <fdeboiss@gmail.com>
2019-11-18 12:48:48 +0100

    remove deprecated tests

--- tests/usecases_tests/refData/use_case_3/input/dart.typ
+++
@@ -1,3 +0,0 @@
-2
-101 125 0 125 Default_Object
-102 0 175 0 Leaf
--- tests/usecases_tests/test_newSimuStruct.py
+++
@@ -1,5 +0,0 @@
-from pytools4dart.simulation import simulation
-
-simu = simulation()
-
-simu.write(modified_simu_name = "newSimuStr")
