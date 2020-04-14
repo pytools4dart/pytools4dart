db39a265a99fdad26c211fcba714f2e3ecfcafb6
floriandeboissieu <fdeboiss@gmail.com>
2020-04-14 13:12:31 +0200

    add ground properties to use_case_3

--- pytools4dart/examples/use_case_3.py
+++ pytools4dart/examples/use_case_3.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import pytools4dart as ptd
+from os.path import join, dirname
 
 # Path of voxelization file
-voxfile = "../data/forest.vox"
+data_dir = join(dirname(ptd.__file__), 'data')
+voxfile = join(data_dir, 'forest.vox')
 
 # create an empty simulation
-simu = ptd.simulation(name = 'use_case_3', empty=True)
+simu = ptd.simulation(name='use_case_3', empty=True)
 
 # set scene size
 simu.scene.size = [20, 20]
@@ -15,25 +17,39 @@ simu.scene.size = [20, 20]
 for wvl in [0.485, 0.555, 0.655]:
     simu.add.band(wvl=wvl, bw=0.07)
 
+# define optical property for ground
+op_ground = {
+    'type':'Lambertian',
+    'ident':'ground',
+    'databaseName':'Lambertian_mineral.db',
+    'ModelName':'clay_brown'}
+
+simu.add.optical_property(**op_ground)
+simu.scene.ground.OpticalPropertyLink.ident='ground'
+
+
 # define optical properties with prospect parameters
 propect_prop = {'CBrown': 0, 'Cab': 30, 'Car': 5,
                 'Cm': 0.01, 'Cw': 0.01, 'N': 1.8,
                 'anthocyanin': 0}
 
-op_name ='op_prospect'
-op_vegetation = {'type':'vegetation',
-              'ident': op_name,
-              'databaseName':'prospect.db',
-              'ModelName':'',
-              'lad': 1,
-              'prospect':propect_prop}
+op_name = 'op_prospect'
+op_vegetation = {'type': 'vegetation',
+                 'ident': op_name,
+                 'databaseName': 'prospect.db',
+                 'ModelName': '',
+                 'lad': 1,
+                 'prospect': propect_prop}
 
 simu.add.optical_property(**op_vegetation)
 
-# add plots from voxelized data
+# read vox file
 vox = ptd.voxreader.voxel().from_vox(voxfile)
-plots = vox.to_plots()
+# Convert vox to DART plots shifting minimum x,y corner to 0,0
+plots = vox.to_plots(reduce_xy=True)
+# add an optical property to each plot
 plots['PLT_OPT_NAME'] = 'op_prospect'
+# add plots to simulation
 simu.add.plots(plots)
 
 # write simulation
@@ -44,4 +60,3 @@ simu.run.full()
 # stack bands
 simu.run.stack_bands()
 simu.run.colorCompositeBands(red=2, green=1, blue=0, iteration='X', outdir='rgb')
-
