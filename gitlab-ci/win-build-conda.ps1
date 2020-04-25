8a899e4e5d8523c483a3f977aa144ecdefca249a
floriandeboissieu <fdeboiss@gmail.com>
2020-04-25 16:03:08 +0200

    update gitlab-ci for windows runner

--- gitlab-ci/win-build-conda.ps1
+++ gitlab-ci/win-build-conda.ps1
@@ -1,7 +1,10 @@
 # If a previous version of ana/miniconda is already there, remove it.
 # Remove the conda section of C:\Users\your_user\Documents\WindowsPowerShell\profile.ps1 if it exists.
 
-$cache = "${env:userprofile}\cache"
+$projectdir = pwd
+echo "$projectdir"
+$cache = "$projectdir\cache"
+echo "$cache"
 $condaurl = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
 $condaexe = "$cache\Miniconda3-latest-Windows-x86_64.exe"
 $condadir = "$cache\miniconda3"
@@ -40,32 +43,6 @@ if(!(Test-Path $condadir -PathType Container)) {
 
 # test pytools4dart dependencies
 $env:PATH = "$condadir\pkgs\openjdk-11.0.1-1018\Library\bin\server;" + $env:PATH
-python -c "import generateDS; import tinyobj; import gdecomp; import laspy"
+python -c "import generateDS; import tinyobj; import gdecomp; import laspy; print('ptdvenv setup done...')"
 
-#### install DART ####
-$dartname = "DART_5-7-6_2020-03-06_v1150_windows64"
-$dartzip = "$cache\$dartname.zip"
-$dartdir = "$cache\$dartname"
-$darturl = "https://dart.omp.eu/membre/downloadDart/contenu/DART/Windows/64bits/$dartname.zip"
-
-if(!(Test-Path $dartdir -PathType Container)) {
-    curl.exe -C - $darturl -o $dartzip
-    tar -xf $dartzip -C $cache
-    python -c "from dart_install_win import install_dart; install_dart(r'$dartdir', r'$dartdir', mode='mv')"
-    ls $dartdir
-}
-
-# install pytools4dart
-pip install .
-
-# test install
-cd pytools4dart\examples
-$env:PATH = "$condadir\pkgs\openjdk-11.0.1-1018\Library\bin\server;" + $env:PATH
-python -c "import generateDS; import tinyobj; import gdecomp; import laspy; import pytools4dart"
-cd $env:userprofile
-
-#### configure DART ####
-cd pytools4dart\examples
-python -c "import pytools4dart as ptd; ptd.configure(r'$dartdir')"
-cd $env:userprofile
 
