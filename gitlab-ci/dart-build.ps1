4c6c9b341d69e01858f20e9ad42d94d930492b7d
floriandeboissieu <fdeboiss@gmail.com>
2020-04-26 21:51:36 +0200

    add install_dart for gitlab-ci

--- gitlab-ci/dart-build.ps1
+++ gitlab-ci/dart-build.ps1
@@ -54,7 +54,7 @@ if(!(Test-Path $prefix -PathType Container)) {
 if(!(Test-Path $dartdir -PathType Container)) {
     curl.exe -C - $darturl -o $dartzip # 8min to download from gitlab.com, <1min to pack/upload cache...
     tar.exe -xf $dartzip -C $prefix # 155s=2min35 to unzip, about the same time to pack/upload cache, 4 min to download/unpack cache
-    python -c "from dart_install_win import install_dart; install_dart(r'$dartdir', mode='mv')"
+    python -c "from pytools4dart.install_dart import install_dart; install_dart(r'$dartdir', mode='mv')"
     Write-Host "Cache content:"
     ls $prefix
     Write-Host "dart content:"
