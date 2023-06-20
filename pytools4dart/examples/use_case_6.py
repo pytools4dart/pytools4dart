693b315ee97137c3b4e82f5bda4bd66859a91de2
Florian de Boissieu <fdeboiss@gmail.com>
2023-06-20 13:20:00 +0200

    fix use_case_6 figure export

--- pytools4dart/examples/use_case_6.py
+++ pytools4dart/examples/use_case_6.py
@@ -169,4 +169,4 @@ axrot.set_xlabel('x')
 axrot.set_ylabel('y')
 axrot.set_title('GIS usual orientation')
 
-fig.savefig(simu.outdir / 'use_case_6.png')
+fig.savefig(simu.output_dir / 'use_case_6.png')
